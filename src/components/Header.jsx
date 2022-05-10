7226544b65fae457499baef42ed4e823db5bb8cc
Karl Stencell <karldstencell@gmail.com>
2022-05-10 19:33:23 -0400

    fixed some spacing issues when viewing on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React, {useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 // import video from '../assets/video.mp4';
-import BraceImage from '../assets/white-brace.png';
+import BraceRight from '../assets/brace-right.png';
+import BraceLeft from '../assets/brace-left.png';
+import WhiteDot from '../assets/white-dot.png';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './CSS/Header.css';
 import Scroll from './Scroll';
@@ -21,48 +23,46 @@ function Header() {
       }, []);
 
     return (
-        <Container className='view'>
-            <Container className='mask'>
-                <Container className='d-flex flex-column justify-content-center header has-bg-img'>
-                    <Container className="h-50 justify-content-center align-items-center">
-                        <Row className='justify-content-center align-items-center h-100'>
-                            <h1 className='text-light titleFont' data-aos="zoom-in" data-aos-delay="400" data-aos-easing="ease">Hi. I'm Karl Stencell</h1>
-                        </Row>
-                    </Container>
-                    <Container className='d-flex flex-row h-40 justify-content-center mt-1'>
-                        <Col className='d-flex flex-column justify-items-right' data-aos="fade-right" data-aos-delay="1000" data-aos-easing="ease">
-                            <div class='curly-brace align-self-end'>&#123;</div>
+        <Container fluid className='view'>
+            <Container fluid className='mask'>
+                <Container fluid className='d-flex flex-column header'>
+                    <Row className='h-40 justify-content-center align-items-center text-light titleFont nowrap' data-aos="zoom-in" data-aos-delay="100" data-aos-easing="ease">
+                        Hi. I'm Karl Stencell
+                    </Row>
+                    <Container className='d-flex flex-row h-40 justify-content-center align-items-stretch'>
+                        <Col className='d-flex align-items-center justify-content-end' data-aos="fade-right" data-aos-delay="600" data-aos-easing="ease" data-aos-duration="1500">
+                            <img src={BraceLeft} className='brace-image brace-image-left align-self-start'/>
                         </Col>
-                        <Col>
+                        <Col className='col-auto'>
                             <Container classname='d-flex flex-column justify-content-center'>
-                                <Row className='justify-content-center'>
-                                <h3 className='text-light titleFont2' data-aos="fade-up" data-aos-delay="1000" data-aos-easing="ease">Software Engineer</h3>
+                                <Row className='justify-content-center titleFont2 text-light nowrap' data-aos="fade-up" data-aos-delay="650" data-aos-easing="ease">
+                                Software Engineer
                                 </Row>
-                                <Row className='justify-content-center'>
-                                <h3 className='text-light titleFont2' data-aos="fade-up" data-aos-delay="1200" data-aos-easing="ease">Computer Scientist</h3>
+                                <Row className='justify-content-center titleFont2 text-light nowrap' data-aos="fade-up" data-aos-delay="700" data-aos-easing="ease">
+                                Computer Scientist
                                 </Row>
-                                <Row className='justify-content-center opacity-75'>
-                                <h3 className='text-light titleFont2' data-aos="fade-up" data-aos-delay="1400" data-aos-easing="ease">Web Designer</h3>
+                                <Row className='justify-content-center titleFont2 text-light nowrap' data-aos="fade-up" data-aos-delay="750" data-aos-easing="ease">
+                                Web Designer
                                 </Row>
-                                <Row className='justify-content-center opacity-75'>
-                                <h3 className='text-light titleFont2' data-aos="fade-up" data-aos-delay="1600" data-aos-easing="ease">Theoretical Physicist</h3>
+                                <Row className='justify-content-center titleFont2 text-light nowrap' data-aos="fade-up" data-aos-delay="800" data-aos-easing="ease">
+                                Theoretical Physicist
                                 </Row>
-                                <Row className='justify-content-center opacity-75'>
-                                <h3 className='text-light titleFont2 dot' data-aos="fade-up" data-aos-delay="1600" data-aos-easing="ease">&#xb7;</h3>
+                                <Row className='justify-content-center' data-aos="fade-up" data-aos-delay="850" data-aos-easing="ease">
+                                <img src={WhiteDot} className='white-dot'/>
                                 </Row>
-                                <Row className='justify-content-center opacity-75'>
-                                <h3 className='text-light titleFont2 dot' data-aos="fade-up" data-aos-delay="1600" data-aos-easing="ease">&#xb7;</h3>
+                                <Row className='justify-content-center' data-aos="fade-up" data-aos-delay="900" data-aos-easing="ease">
+                                <img src={WhiteDot} className='white-dot'/>
                                 </Row>
-                                <Row className='justify-content-center opacity-75'>
-                                <h3 className='text-light titleFont2 dot' data-aos="fade-up" data-aos-delay="1600" data-aos-easing="ease">&#xb7;</h3>
+                                <Row className='justify-content-center' data-aos="fade-up" data-aos-delay="950" data-aos-easing="ease">
+                                <img src={WhiteDot} className='white-dot'/>
                                 </Row>
                             </Container>
                         </Col>
-                        <Col data-aos="fade-left" data-aos-delay="1000" data-aos-easing="ease">
-                            <div class='curly-brace align-self-start'>&#125;</div>
+                        <Col data-aos="fade-left" data-aos-delay="600" data-aos-easing="ease" data-aos-duration="1500">
+                            <img src={BraceRight} className='brace-image brace-image-right'/>
                         </Col>
                     </Container>
-                    <Container className="d-flex flex-column h-25 justify-content-around" data-aos="fade-up" data-aos-delay="2000" data-aos-easing="ease">
+                    <Container className="d-flex flex-column h-20 justify-content-around" data-aos="fade-up" data-aos-delay="1000" data-aos-easing="ease">
                         <Row className='justify-content-center'>
                             <h5 class='text-light titleFont3 pt-3'>SCROLL DOWN</h5>
                         </Row>
